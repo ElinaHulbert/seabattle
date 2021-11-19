f532f7a2e2790ad73b8bd613fa72264527c3bbbb
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2021-11-19 15:36:05 +0100

    added ship wich is moving within the grid

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,7 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.image("waves", "./assets/waves.jfif");
+  this.load.image("ship3", "./assets/3_ship.png");
   // this.load.spritesheet('dude',
   //       'assets/dude.png',
   //       { frameWidth: 32, frameHeight: 48 }
@@ -28,17 +29,25 @@ function create() {
 
   // grid
   var grid = this.add.grid(
-    300,
-    300,
-    320,
-    320,
-    32,
-    32,
+    400, //x
+    300, //y
+    320, //width
+    320, //height
+    32, //cellWidth
+    32, //cellHeight
     0xdffbff,
     1,
     0x000000,
     1
   );
+
+  // this.aGrid = new AlignGrid({scene:this,rows:11,cols:11});
+  // this.aGrid.showNumbers
+
+  var x = Phaser.Math.Between(253, 547);
+  var y = Phaser.Math.Between(188, 412);
+  var ship3 = this.add.image(x, y, "ship3");
+  ship3.setScale(0.8, 0.7);
 }
 
 function update() {}
