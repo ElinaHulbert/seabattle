09f42d74345ece7cc25e4a3565378fd8394a2d3a
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2021-11-29 13:43:30 +0100

    fixed overlapping of the two sell ship

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,6 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-  
   this.load.image("waves", "./assets/pattern.jpg");
   this.load.image("ship1", "./assets/1_ship.png");
   this.load.image("ship2", "./assets/2_ship.png");
@@ -70,17 +69,20 @@ function create() {
   let ship1_y_size = 35;
   let splash_y_size = 78;
   let total_number_ships1 = 4;
-  let total_number_splash = cell_quantity * cell_quantity; 
+  let total_number_splash = cell_quantity * cell_quantity;
 
-
-  for(let splash_number = 0; splash_number < total_number_splash; splash_number++) {
+  for (
+    let splash_number = 0;
+    splash_number < total_number_splash;
+    splash_number++
+  ) {
     let k = Phaser.Math.Between(0, cell_quantity - 1);
     let l = Phaser.Math.Between(0, cell_quantity - 1);
     while (splash_config.some((splash) => splash[0] == k && splash[1] == l)) {
       k = Phaser.Math.Between(0, cell_quantity - 1);
       l = Phaser.Math.Between(0, cell_quantity - 1);
     }
-   
+
     // console.log(ships_config, " ships_config")
     splash_config.push([k, l]);
 
@@ -98,7 +100,7 @@ function create() {
       this.alpha = 1;
     });
   }
-  
+
   let i;
   let j;
 
@@ -116,7 +118,6 @@ function create() {
 
     //ship position is stored in array "ships_config"
     ships_config.push([i, j]);
-    ships_config.push([i, j+1]);
 
     var ship1 = this.add.sprite(
       x_0 + cell_size * (i + 0.5),
@@ -124,14 +125,12 @@ function create() {
       "ship1"
     );
 
-   
     //ship image is scaled to fit into the cell
     ship1.setScale((0.9 * cell_size) / ship1_y_size);
     ship1.setInteractive();
 
     ship1.alpha = 0.000001;
     this.input.enableDebug(ship1);
-   
 
     ship1.on("pointerdown", function () {
       if (this.alpha != 1) {
@@ -141,21 +140,25 @@ function create() {
       }
     });
   }
-  
 
   for (let ship_number = 0; ship_number < 3; ship_number++) {
     let i = Phaser.Math.Between(0, cell_quantity - 1);
     let j = Phaser.Math.Between(0, cell_quantity - 2);
 
-    while (ships_config.some((ship) => ship[0] == i && ship[1] == j)) {
+    while (
+      ships_config.some(
+        (ship) =>
+          (ship[0] == i && ship[1] == j) || (ship[0] == i && ship[1] == j + 1)
+      )
+    ) {
       i = Phaser.Math.Between(0, cell_quantity - 1);
       j = Phaser.Math.Between(0, cell_quantity - 2);
     }
-    
-   //ship position is stored in array "ships_config"
+
+    //ship position is stored in array "ships_config"
     ships_config.push([i, j]);
-    
-   
+    ships_config.push([i, j + 1]);
+
     var ship2 = this.add.sprite(
       x_0 + cell_size * (i + 0.5),
       y_0 + cell_size * (j + 1),
@@ -174,16 +177,19 @@ function create() {
         score++;
         // this.alpha = 1;
 
-        let i = Math.floor((scene.input.activePointer.x - x_0)/cell_size);
+        let i = Math.floor((scene.input.activePointer.x - x_0) / cell_size);
         let j = Math.floor((scene.input.activePointer.y - y_0) / cell_size);
 
-        scene.add.sprite(x_0 + cell_size * (i + 0.5),
-        y_0 + cell_size * ((j + 0.5)), "flower")
+        scene.add.sprite(
+          x_0 + cell_size * (i + 0.5),
+          y_0 + cell_size * (j + 0.5),
+          "flower"
+        );
+
         scoreText.setText("score: " + score);
       }
     });
   }
-
 }
 
 // function update() {
@@ -191,7 +197,6 @@ function create() {
 //     scoreText.setText("Score: " + score);
 //   }
 // }
-
 
 //VERSION 1
 //user precess on one cell
@@ -205,7 +210,7 @@ function create() {
 //VERSION 2
 //add two flowers instead of the each two-cell ships (one flower per each ship)
 //repeat 2 times:
-   //user precess on one cell
-   //if the cell is taken by a two-cell ship,
-   //change the opacity of the flower from 0 to 1
+//user precess on one cell
+//if the cell is taken by a two-cell ship,
+//change the opacity of the flower from 0 to 1
 //add an animation that the two-cell ship will apear one second after the user found out both cells of the two-cell ship
